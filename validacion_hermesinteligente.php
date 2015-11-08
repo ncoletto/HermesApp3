111d29fe380a4a7eb08517e7d02bf5f6ea23e8ce
ncoletto <ncoletto@gmail.com>
2015-11-08 19:28:29 -0300

    HermesInteligente
    HermesIngeligente

---
+++ validacion_hermesinteligente.php
@@ -0,0 +1,52 @@
+<?php
+
+
+$conexion = mysqli_connect("localhost", "hermesin_root", "Hermesinteligente5", "hermesin_basehermes");
+
+/* Extrae los valores enviados desde la aplicacion movil */
+$usuarioEnviado = $_GET['usuario'];
+$passwordEnviado = $_GET['password'];
+
+/* revisar existencia del usuario con la contraseña en la bd */
+$sqlCmd = "SELECT * FROM usuarios WHERE nombreUsuario LIKE '".mysqli_real_escape_string($conexion,$usuarioEnviado)."' AND claveUsuario='".mysqli_real_escape_string($conexion,$passwordEnviado)."' LIMIT 1";
+$sqlQry = mysqli_query($conexion,$sqlCmd);
+if(mysqli_num_rows($sqlQry)>0)
+{
+$login=1;
+}
+else
+{
+$login=0;
+}
+
+/* Define los valores que seran evaluados, en este ejemplo son valores estaticos,
+en una verdadera aplicacion generalmente son dinamicos a partir de una base de datos */
+
+/* Extrae los valores enviados desde la aplicacion movil */
+$usuarioEnviado = $_GET['usuario'];
+$passwordEnviado = $_GET['password'];
+
+/* crea un array con datos arbitrarios que seran enviados de vuelta a la aplicacion */
+$resultados = array();
+$resultados["hora"] = date("F j, Y, g:i a");
+$resultados["generador"] = "www.hermesinteligente.com" ;
+
+/* verifica que el usuario y password concuerden correctamente */
+if( $login==1 ){
+/*esta informacion se envia solo si la validacion es correcta */
+$resultados["mensaje"] = "Validacion Correcta";
+$resultados["validacion"] = "ok";
+
+}else{
+/*esta informacion se envia si la validacion falla */
+$resultados["mensaje"] = "Usuario / password incorrectos";
+$resultados["validacion"] = "error";
+}
+
+/*convierte los resultados a formato json*/
+$resultadosJson = json_encode($resultados);
+
+/*muestra el resultado en un formato que no da problemas de seguridad en browsers */
+echo $_GET['jsoncallback'] . '(' . $resultadosJson . ');';
+
+?>
